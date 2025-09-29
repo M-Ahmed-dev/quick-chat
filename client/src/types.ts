3d5e484d4dd8d23e6f7d9d0809fbfcaada067b8d
m-ahmad-dev <winterfell551@gmail.com>
2025-09-29 06:08:52 -0700

    completed frontend of chat app

---
+++ client/src/types.ts
@@ -0,0 +1,23 @@
+export type User = {
+  _id: string;
+  email: string;
+  fullName: string;
+  profilePic: string;
+  bio: string;
+};
+
+export type IconsType = {
+  avatar_icon: string;
+  gallery_icon: string;
+  help_icon: string;
+  logo_big: string;
+  logo_icon: string;
+  logo: string;
+  search_icon: string;
+  send_button: string;
+  menu_icon: string;
+  arrow_icon: string;
+  code: string;
+  bgImage: string;
+  profile_martin: string;
+};
